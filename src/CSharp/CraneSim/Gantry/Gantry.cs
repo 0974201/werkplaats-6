56fd87bc284d8763c9bce5bf7eca2f1e1a56a6d1
Sushi <31491170+0974201@users.noreply.github.com>
2023-12-05 20:20:10 +0100

    added default values, and missing semi-colons #14

--- src/CSharp/CraneSim/Gantry/Gantry.cs
+++ src/CSharp/CraneSim/Gantry/Gantry.cs
@@ -9,19 +9,30 @@ namespace Gantry
     public class Gantry
     {
 
-        public string Component { get; set }
+        public string Component { get; set; } = "Gantry"; //default value
 
-        public float PositionZ { get; set }
+        public float PositionZ { get; set; } = 0; //ik denk dat ik double moet gebruiken hier ipv float?
 
-        public float Speed { get; set; }
-        public float Acceleration { get; set; }
+        public float Speed { get; set; } = 0;
+        public float Acceleration { get; set; } = 0;
 
-        public bool IsActive {  get; set; }
+        public bool IsActive { get; set; } = false;
 
 
         public Gantry()
         {
 
         }
+
+        /*static void Main(string[] args)
+        {
+            Gantry gantry = new Gantry();
+            
+            Console.WriteLine(gantry.Component); //does this work?
+            Console.WriteLine(gantry.PositionZ);
+            Console.WriteLine(gantry.Speed);
+            Console.WriteLine(gantry.Acceleration);
+            Console.WriteLine(gantry.IsActive);
+        }*/
     }
 }
