441c4c284e04c2213d4fffd63080f21634e66109
Sushi <31491170+0974201@users.noreply.github.com>
2023-12-05 15:20:43 +0100

    adding gantry.cs #14

---
+++ src/CSharp/CraneSim/Gantry/Gantry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gantry
+{
+    public class Gantry
+    {
+
+        public string Component;
+
+        public float PositionZ;
+
+        public float Speed;
+        public float Acceleration;
+
+        public bool IsActive;
+
+
+        public Gantry()
+        {
+        }
+    }
+}
