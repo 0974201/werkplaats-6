7b23ab27846f9068836314f632bd29a30e1ed378
Andy Vandewalle <113889343+AndyHowest@users.noreply.github.com>
2023-12-07 10:15:01 +0100

    Add TrolleyService.cs #3

---
+++ src/CSharp/CraneSim/CraneSim.Core/Services/TrolleyService.cs
@@ -0,0 +1,95 @@
+﻿using CraneSim.Core.Entities;
+using CraneSim.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CraneSim.Core.Services
+{
+    public class TrolleyService : ITrolleyService
+    {
+        Stopwatch _trolleyMoveStopwatch;
+
+        public TrolleyService()
+        {
+            _trolleyMoveStopwatch = new Stopwatch();
+        }
+
+        public async Task<float> CalculateConstantAccelaration(Trolley entity)
+        {
+            var accelerationTime = entity.AccelAndDecelarationTime;
+            var currentSpeed = entity.Speed;
+            var topspeed = entity.MaximumSpeedValue;
+
+            float result = (topspeed - currentSpeed) / accelerationTime;
+
+            entity.Acceleration = result;
+            return result;
+        }
+
+        public async Task<float> CalculateCurrentSpeed(Trolley entity)
+        {
+            var timePast = (float)(_trolleyMoveStopwatch.ElapsedMilliseconds / 1000);
+
+            if (timePast < entity.AccelAndDecelarationTime)
+            {
+                entity.Speed = entity.Acceleration * timePast;
+            }
+            else if (timePast < 2 * entity.AccelAndDecelarationTime)
+            {
+                entity.Speed = entity.MaximumSpeedValue - entity.Deceleration * (timePast - entity.AccelAndDecelarationTime);
+            }
+            else
+            {
+                entity.Speed = 0;
+            }
+
+            entity.Speed = Math.Max(entity.MinimumSpeedValue, Math.Min(entity.MaximumSpeedValue,entity.Speed));
+
+            return entity.Speed;
+        }
+
+        public async Task<float> CalculateHorizontaleNegatiefMovement(Trolley entity)
+        {
+
+            var timePast = (float)(_trolleyMoveStopwatch.ElapsedMilliseconds / 1000.0);
+            var currentSpeed = entity.Speed;
+            var travelledDistance = currentSpeed * timePast;
+
+            float newPositionX = entity.PositionX - travelledDistance;
+
+            return newPositionX;
+        }
+
+        public async Task<float> CalculateHorizontalePositiefMovement(Trolley entity)
+        {
+            var timePast = (float)(_trolleyMoveStopwatch.ElapsedMilliseconds / 1000.0);
+            var currentSpeed = entity.Speed;
+            var travelledDistance = currentSpeed * timePast;
+
+            float newPositionX = entity.PositionX + travelledDistance;
+
+            return newPositionX;
+        }
+
+        public async Task ResetStopWatch()
+        {
+            _trolleyMoveStopwatch.Reset();
+        }
+
+        public async Task StartStopwatch()
+        {
+            _trolleyMoveStopwatch.Start();
+        }
+
+        public async Task StopStopwatch()
+        {
+            _trolleyMoveStopwatch.Stop();
+        }
+    }
+}
