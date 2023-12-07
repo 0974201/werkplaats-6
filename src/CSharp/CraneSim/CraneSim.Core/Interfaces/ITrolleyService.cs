35c5052e1bece8ff8ee147c23b7001199a5a5172
Andy Vandewalle <113889343+AndyHowest@users.noreply.github.com>
2023-12-07 10:05:33 +0100

    Add ITrolleyService.cs #3

---
+++ src/CSharp/CraneSim/CraneSim.Core/Interfaces/ITrolleyService.cs
@@ -0,0 +1,20 @@
+﻿using CraneSim.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CraneSim.Core.Interfaces
+{
+    public interface ITrolleyService
+    {
+        Task<float> CalculateHorizontalePositiefMovement(Trolley entity);
+        Task<float> CalculateHorizontaleNegatiefMovement(Trolley entity);
+        Task<float> CalculateCurrentSpeed(Trolley entity);
+        Task<float> CalculateConstantAccelaration(Trolley entity);
+        Task ResetStopWatch();
+        Task StartStopwatch();
+        Task StopStopwatch();
+    }
+}
