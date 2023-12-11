78e6802cfcb26642006a31b900bab6418d18dc8a
Sushi <31491170+0974201@users.noreply.github.com>
2023-12-11 09:19:21 +0100

    Added Gantry.cs

---
+++ src/CSharp/CraneSim/CraneSim.Core/Entities/Gantry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CraneSim.Core.Entities
+{
+    public class Gantry
+    {
+        public string Component { get; set; } = "Gantry"; //default value
+
+        public float PositionZ { get; set; } = 0.0F; //ik denk dat ik double moet gebruiken hier ipv float?
+
+        public float Speed { get; set; } = 0.0F;
+        public float Acceleration { get; set; } = 0.0F;
+
+        public bool IsActive { get; set; } = false;
+    }
+}
