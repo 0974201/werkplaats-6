28d87a0e7c8495faa911abcb31db77a9dab2be6e
Quylian Hoogstad <quylian.hoogstad@gmail.com>
2023-12-12 10:45:34 +0100

    hoist movement

--- src/Python/Microservices/hoist.py
+++ src/Python/Microservices/hoist.py
@@ -1,20 +1,33 @@
+import time
 class Hiost:
-    def is_active():
-        is_active = False
-        return is_active
+    def __init__(self):
+        self.acceleration = 1
+        self.is_active = True
+        self.hoist_y = 500
 
-    while is_active():
-        def position():
-            start_hoist_y = 500
-            move_hoist_y = 0  # placeholder totdat movement is gemaakt
-            new_hoist_y = 0   # placeholder totdat movement is gemaakt
+    def movement(self):
+        while self.is_active:
+            time.sleep(0.25)
+            
+            self.acceleration = self.acceleration * 1.1
+            speed = 0.83 * self.acceleration
 
-        def container_connect():
-            # placeholders totdat de container interactie is gemaakt
-            is_connected = False
-            can_connect = False
+            if speed >= 2.5:
+                speed = 2.5
+            # print(speed)
+            move_hoist_y = speed
+            self.position(move_hoist_y)
 
-        def movement():
-            # placeholder totdat de movement is gemaakt
-            speed = 0
-            acceleration = 0
+    def position(self, move_hoist_y):
+        self.hoist_y += move_hoist_y
+        print(self.hoist_y)
+
+    def container_connect(self):
+        # placeholders until the container interaction is implemented
+        is_connected = False
+        can_connect = False
+
+
+hiost_instance = Hiost()
+
+hiost_instance.movement()
