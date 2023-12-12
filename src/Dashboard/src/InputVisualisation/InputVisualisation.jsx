5b4b59bea31eaf6381481522d1f5a830ce7380cb
Zasha <0925315@hr.nl>
2023-12-12 12:05:38 +0100

    add Button #25

--- src/Dashboard/src/InputVisualisation/InputVisualisation.jsx
+++ src/Dashboard/src/InputVisualisation/InputVisualisation.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import './InputVisualisation.css'
+import EmergencyButton from "../EmergencyButton/EmergencyButton.jsx";
 
 export default function InputVisualisation() {
 
+    function Button(props) {
+
+        return (
+            <div id={"inputButton"} style={{backgroundColor: "red"}}>
+                <span>{props.letter}</span>
+            </div>
+        )
+    }
+
     return (
         <>
-
+            <EmergencyButton />
+            <Button letter={"W"} active={true} />
         </>
     )
 }
