e31294ead07221d831a5616788743160989e13a4
Andy Vandewalle <113889343+AndyHowest@users.noreply.github.com>
2023-12-12 16:00:18 +0100

    Add ITrolleyMicroService.cs #37

---
+++ src/CSharp/CraneSim/CraneSim.Core/Interfaces/ITrolleyMicroSevice.cs
@@ -0,0 +1,22 @@
+﻿using CraneSim.Core.Entities;
+using HiveMQtt.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CraneSim.Core.Interfaces
+{
+    public interface ITrolleyMicroSevice
+    {
+        Task EstablishBrokerConnection();
+        Task DisconnectBrokerConnection();
+        Task SendMessage();
+        void Client_OnMessageReceived(object sender, OnMessageReceivedEventArgs e);
+        void Client_OnDisconnectRecieved(object sender, OnDisconnectReceivedEventArgs e);
+        void Client_BeforeConnect(object sender, BeforeConnectEventArgs e);
+        void Client_AfterConnect(object sender, AfterConnectEventArgs e);
+
+    }
+}
