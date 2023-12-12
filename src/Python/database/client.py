be310f2415c09f5aa309c95b3ccd9cc9f1779ece
miroaccgh <mironoordzij@outlook.comD>
2023-12-12 21:11:25 +0100

    refactored database; correct names, deleted old comments

--- src/Python/database/client.py
+++ src/Python/database/client.py
@@ -3,26 +3,13 @@ from dotenv import load_dotenv
 import os
 
 
-def get_environment_variables():
-    load_dotenv()
-    return {
-        "DATABASE_URL": os.getenv("DATABASE_URL")
-    }
-
-
 class Database:
     def __init__(self) -> None:
-        # get environment variables
-        self.environment_variables = get_environment_variables()
-        # create and assign a client to the running mongod instance to property
-        self.client = MongoClient(
-            self.environment_variables["DATABASE_URL"],
-            tls=True,
-            tlsCertificateKeyFile="certificates/admin.pem"
-        )
-        # assign, and create if doesn't exist, database to property
+        load_dotenv()
+        self.uri = os.getenv("URI")
+        self.certificate = os.path.join(os.path.dirname(__file__), "certificate/admin.pem")
+        self.client = MongoClient(self.uri, tls=True, tlsCertificateKeyFile=self.certificate)
         self.database = self.client["st-2324-1-d-wx1-t2-2324-wx1-bear"]
-        # assign, and create if doesn't exist, collection to property
         self.collection = self.database.crane_state
 
     def insert_document(self, document) -> dict:
