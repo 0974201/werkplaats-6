436ca78ada0edd5cc9cf76504b06777865b176cd
Sushi <31491170+0974201@users.noreply.github.com>
2023-12-12 22:12:16 +0100

    Added IGantryMicroservice.cs #38

---
+++ src/CSharp/CraneSim/CraneSim.Core/Interfaces/IGantryMicroservice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HiveMQtt.Client.Events;
+
+namespace CraneSim.Core.Interfaces
+{
+    public interface IGantryMicroservice
+    {
+        Task EstablishBrokerConnection();
+        Task SendMessage();
+        Task DisconnectBrokerConnection();
+        
+        void Client_BeforeConnect(object sender, BeforeConnectEventArgs e);
+        void Client_AfterConnect(object sender, AfterConnectEventArgs e);
+        void Client_OnMessageReceived(object sender, OnMessageReceivedEventArgs e);
+        void Client_OnDisconnectRecieved(object sender, OnDisconnectReceivedEventArgs e);
+    }
+}
