a619232c295b1cc28460c6ed8c3818bbd34cf2fa
quylian <90244189+TOA4ASA@users.noreply.github.com>
2023-12-13 09:04:46 +0100

    init and self values

--- src/Python/Microservices/hoist.py
+++ src/Python/Microservices/hoist.py
@@ -1,20 +1,19 @@
-class Hiost:
-    def is_active():
-        is_active = False
-        return is_active
+class Hoist:
+    def __init__(self):
+        self.is_active = False
 
-    while is_active():
-        def position():
-            start_hoist_y = 500
-            move_hoist_y = 0  # placeholder totdat movement is gemaakt
-            new_hoist_y = 0   # placeholder totdat movement is gemaakt
+    def position(self):
+        start_hoist_y = 500
+        move_hoist_y = 0  # placeholder totdat movement is gemaakt
+        new_hoist_y = 0   # placeholder totdat movement is gemaakt
 
-        def container_connect():
-            # placeholders totdat de container interactie is gemaakt
-            is_connected = False
-            can_connect = False
+    def container_connect(self):
+        # placeholders totdat de container interactie is gemaakt
+        is_connected = False
+        can_connect = False
 
-        def movement():
-            # placeholder totdat de movement is gemaakt
-            speed = 0
-            acceleration = 0
+    def movement(self):
+        # placeholder totdat de movement is gemaakt
+        speed = 0
+        acceleration = 0
+
