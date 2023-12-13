ad08a00db0a2152c5282a456d68e63c89829a39b
Yashin <1027048@hr.nl>
2023-12-13 13:50:52 +0100

    added parameter angle #15

--- src/Python/Microservices/boom.py
+++ src/Python/Microservices/boom.py
@@ -6,13 +6,14 @@ import json
 # define class boom 
 class Boom:
   # constructor method for the class
-  def __init__(self, isActive, Speed, positionX, positionY, rotationZ):
+  def __init__(self, isActive, Speed, positionX, positionY, rotationZ, angle):
     # assign the parameters to the instance attributes
     self.isActive = isActive 
     self.Speed = Speed 
     self.positionX = positionX 
     self.positionY = positionY 
     self.rotationZ = rotationZ 
+    self.angle = angle
     
 # method for getting the cordinates
   def get_coordinates(self):
