95103b1c434d2ad376c64c7b3d480163fe79e152
Andy Vandewalle <113889343+AndyHowest@users.noreply.github.com>
2023-12-13 09:20:28 +0100

    Update TrolleyService.cs #37

--- src/CSharp/CraneSim/CraneSim.Core/Services/TrolleyService.cs
+++ src/CSharp/CraneSim/CraneSim.Core/Services/TrolleyService.cs
@@ -1,5 +1,7 @@
 ﻿using CraneSim.Core.Entities;
 using CraneSim.Core.Interfaces;
+using HiveMQtt.Client.Options;
+using HiveMQtt.Client;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -8,6 +10,9 @@ using System.Net.WebSockets;
 using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
+using HiveMQtt.Client.Events;
+using CraneSim.Core.Dtos;
+using System.Text.Json;
 
 namespace CraneSim.Core.Services
 {
@@ -15,9 +20,26 @@ namespace CraneSim.Core.Services
     {
         public readonly Stopwatch _trolleyMoveStopwatch;
 
-        public TrolleyService()
+        HiveMQClient _client;
+        HiveMQClientOptions _options;
+        Trolley _activeTrolley;
+        public TrolleyService(Trolley activeTrolley)
         {
             _trolleyMoveStopwatch = new Stopwatch();
+
+            //=====================================================================
+
+            _options = new HiveMQClientOptions();
+            _options.Host = "c0bbe3829ad14fe3b24e5c51247f57c1.s2.eu.hivemq.cloud";
+            _options.Port = 8883;
+            _options.UseTLS = true;
+            _options.UserName = "cranemqtt";
+            _options.Password = "7va@tWTv2.Jw2yk";
+
+            _client = new HiveMQClient(_options);
+
+            _activeTrolley = activeTrolley;
+
         }
 
         public float CalculateConstantAccelaration(Trolley entity)
@@ -105,6 +127,60 @@ namespace CraneSim.Core.Services
         public double ReturnStopwatchvalue()
         {
             return (double)(_trolleyMoveStopwatch.ElapsedMilliseconds) / 1000;
+        }
+
+        //=====================================================================
+
+        public async Task EstablishBrokerConnection()
+        {
+            var connectResult = await _client.ConnectAsync().ConfigureAwait(false);
+
+            //subscribe
+            await _client.SubscribeAsync("").ConfigureAwait(false);
+
+            _client.OnMessageReceived += Client_OnMessageReceived;
+            _client.OnDisconnectReceived += Client_OnDisconnectRecieved;
+            _client.BeforeConnect += Client_BeforeConnect;
+            _client.AfterConnect += Client_AfterConnect;
+        }
+
+        public async Task DisconnectBrokerConnection()
+        {
+            await _client.DisconnectAsync().ConfigureAwait(false);
+        }
+
+        public async Task SendMessage()
+        {
+            // Publish
+            await _client.PublishAsync("", "").ConfigureAwait(false);
+        }
+
+        public void Client_OnMessageReceived(object sender, OnMessageReceivedEventArgs e)
+        {
+            //if you have recieved a message you can find it here, and do stuff when OnMessageRecieved.
+            var payload = e.PublishMessage.PayloadAsString;
+            TrolleyRequestDto trolleyRequestDto = JsonSerializer.Deserialize<TrolleyRequestDto>(payload);
+
+            _activeTrolley.IsActive = trolleyRequestDto.IsActive;
+            _activeTrolley.Name = trolleyRequestDto.Name;
+            _activeTrolley.Speed = trolleyRequestDto.Speed;
+            _activeTrolley.Acceleration = trolleyRequestDto.Acceleration;
+            _activeTrolley.PositionX = trolleyRequestDto.PositionX;
+        }
+
+        public void Client_OnDisconnectRecieved(object sender, OnDisconnectReceivedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Client_BeforeConnect(object sender, BeforeConnectEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Client_AfterConnect(object sender, AfterConnectEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
