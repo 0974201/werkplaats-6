a2cf12529f5f7ceacc14058e68a8c343084eeb52
quylian <90244189+TOA4ASA@users.noreply.github.com>
2023-12-13 09:38:48 +0100

    deceleration function

--- src/Python/Microservices/hoist.py
+++ src/Python/Microservices/hoist.py
@@ -1,14 +1,17 @@
 import time
-class Hiost:
+
+
+class Hoist:
     def __init__(self):
         self.acceleration = 1
         self.is_active = True
         self.hoist_y = 500
+        self.move = False  # changes als mqtt is geconnect
 
-    def postive_movement(self):
+    def positive_movement(self):
         while self.is_active:
             time.sleep(0.25)
-            
+
             self.acceleration = self.acceleration * 1.1
             speed = 0.83 * self.acceleration
 
@@ -16,18 +19,33 @@ class Hiost:
                 speed = 2.5
             # print(speed)
             move_hoist_y = speed
+            self.deceleration(speed)
             self.position(move_hoist_y)
-    
-    def negetive_movement(self):
+
+    def negative_movement(self):
         while self.is_active:
+            while self.move:
+                time.sleep(0.25)
+
+                self.acceleration = self.acceleration * 1.1
+                speed = -0.83 * self.acceleration
+
+                if speed <= -2.5:
+                    speed = -2.5
+                print(speed)
+                move_hoist_y = speed
+                self.deceleration(speed)
+                self.position(move_hoist_y)
+
+    def deceleration(self, speed):
+        while not self.move:
             time.sleep(0.25)
-
-            self.acceleration = self.acceleration * 1.1
-            speed = -0.83 * self.acceleration
-
-            if speed <= -2.5:
-                speed = -2.5
-            print(speed)
+            if -0.5 <= speed <= 0.5:
+                speed = 0
+            elif speed >= 0.5:
+                speed -= 0.2
+            else:
+                speed += 0.2
             move_hoist_y = speed
             self.position(move_hoist_y)
 
@@ -39,8 +57,3 @@ class Hiost:
         # placeholders until the container interaction is implemented
         is_connected = False
         can_connect = False
-
-
-hiost_instance = Hiost()
-
-hiost_instance.negetive_movement()
