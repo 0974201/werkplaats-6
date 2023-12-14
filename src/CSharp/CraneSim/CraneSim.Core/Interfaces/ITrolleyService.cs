d53f61f4e8b167d7d613ddf807dec2939c90986b
Andy Vandewalle <113889343+AndyHowest@users.noreply.github.com>
2023-12-14 09:38:29 +0100

    Update ITrolleyService.cs #37

--- src/CSharp/CraneSim/CraneSim.Core/Interfaces/ITrolleyService.cs
+++ src/CSharp/CraneSim/CraneSim.Core/Interfaces/ITrolleyService.cs
@@ -10,23 +10,21 @@ namespace CraneSim.Core.Interfaces
 {
     public interface ITrolleyService
     {
-        float CalculateHorizontalPositiveMovement(Trolley entity);
-        float CalculateHorizontalNegativeMovement(Trolley entity);
-        float CalculateCurrentSpeed(Trolley entity);
-        float CalculateConstantAccelaration(Trolley entity);
+        float CalculateHorizontalPositiveMovement();
+        float CalculateHorizontalNegativeMovement();
+        float CalculateCurrentSpeed();
+        float CalculateConstantAccelaration();
         void ResetStopWatch();
         void StartStopwatch();
         void StopStopwatch();
         double ReturnStopwatchvalue();
 
-        //===================================================================
+        //=========== ⬇⬇⬇⬇ Mqtt code ⬇⬇⬇⬇ ===============
+
         Task EstablishBrokerConnection();
         Task DisconnectBrokerConnection();
         Task SendMessage();
         void Client_OnMessageReceived(object sender, OnMessageReceivedEventArgs e);
-        void Client_OnDisconnectRecieved(object sender, OnDisconnectReceivedEventArgs e);
-        void Client_BeforeConnect(object sender, BeforeConnectEventArgs e);
-        void Client_AfterConnect(object sender, AfterConnectEventArgs e);
-
+        
     }
 }
