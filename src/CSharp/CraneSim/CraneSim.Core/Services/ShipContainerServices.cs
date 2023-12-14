36e14e79c1a3a1d91a2ca54356e90411c4298a83
Andy Vandewalle <113889343+AndyHowest@users.noreply.github.com>
2023-12-14 11:26:01 +0100

    Add ShipContainerService.cs #42

---
+++ src/CSharp/CraneSim/CraneSim.Core/Services/ShipContainerServices.cs
@@ -0,0 +1,54 @@
+﻿using CraneSim.Core.Entities;
+using CraneSim.Core.Interfaces;
+using HiveMQtt.Client;
+using HiveMQtt.Client.Events;
+using HiveMQtt.Client.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CraneSim.Core.Services
+{
+    public class ShipContainerServices : IShipContainerService
+    {
+        HiveMQClient _client;
+        HiveMQClientOptions _options;
+        private readonly Shipcontainer _activeShipContainer;
+
+        public ShipContainerServices(Shipcontainer activeShipContainer)
+        {
+            _options = new HiveMQClientOptions();
+            _options.Host = "c0bbe3829ad14fe3b24e5c51247f57c1.s2.eu.hivemq.cloud";
+            _options.Port = 8883;
+            _options.UseTLS = true;
+            _options.UserName = "ShipContainerMqtt"; // to check with hivemq
+            _options.Password = "7va@tWTv2.Jw2yk";
+
+            _client = new HiveMQClient(_options);
+
+            _activeShipContainer = activeShipContainer;
+        }
+
+        public Task EstablishBrokerConnection()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task DisconnectBrokerConnection()
+        {
+            bool disconnectResult = await _client.DisconnectAsync().ConfigureAwait(false);
+        }
+
+        public void Client_OnMessageReceived(object sender, OnMessageReceivedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendMessage()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
