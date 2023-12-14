11fc8fdb4e715938950d6d9b3b0c72fd9dddd07a
Andy Vandewalle <113889343+AndyHowest@users.noreply.github.com>
2023-12-14 11:25:34 +0100

    Add IShipContainerService.cs #42

---
+++ src/CSharp/CraneSim/CraneSim.Core/Interfaces/IShipContainerService.cs
@@ -0,0 +1,19 @@
+﻿using HiveMQtt.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CraneSim.Core.Interfaces
+{
+    public interface IShipContainerService
+    {
+        //=========== ⬇⬇⬇⬇ Mqtt code ⬇⬇⬇⬇ ===============
+
+        Task EstablishBrokerConnection();
+        Task DisconnectBrokerConnection();
+        Task SendMessage();
+        void Client_OnMessageReceived(object sender, OnMessageReceivedEventArgs e);
+    }
+}
