20f1ca3aa8b7a818f56177a0de741a0b9c936642
Andy Vandewalle <113889343+AndyHowest@users.noreply.github.com>
2023-12-15 10:53:35 +0100

    Add HoistResponseDtos #42

---
+++ src/CSharp/CraneSim/CraneSim.Core/Dtos/Hoist/HoistResponseMsgDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace CraneSim.Core.Dtos.Hoist
+{
+    public class HoistResponseMsgDto
+    {
+        [JsonPropertyName("relativePosition")]
+        public HoistResponseRelPosDto RelativePosition { get; set; }
+    }
+}
