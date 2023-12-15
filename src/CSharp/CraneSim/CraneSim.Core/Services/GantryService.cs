a03db6859f4e85433d9c1b551d604a67c85eec97
Sushi <31491170+0974201@users.noreply.github.com>
2023-12-15 21:12:44 +0100

    Updated GantryService.cs #38

--- src/CSharp/CraneSim/CraneSim.Core/Services/GantryService.cs
+++ src/CSharp/CraneSim/CraneSim.Core/Services/GantryService.cs
@@ -6,7 +6,9 @@ using System.Text;
 using System.Threading.Tasks;
 using CraneSim.Core.Entities;
 using CraneSim.Core.Interfaces;
+using HiveMQtt.Client;
 using HiveMQtt.Client.Events;
+using HiveMQtt.Client.Options;
 
 namespace CraneSim.Core.Services
 {
@@ -14,9 +16,26 @@ namespace CraneSim.Core.Services
     {
         public readonly Stopwatch _gantryMoveStopwatch;
 
-        public GantryService()
+        HiveMQClient _client;
+        HiveMQClientOptions _options;
+        public readonly Gantry _activeGantry;
+
+        public GantryService(Gantry activeGantry)
         {
             _gantryMoveStopwatch = new Stopwatch();
+
+            /* mqtt cred */
+            _options = new HiveMQClientOptions();
+            _options.Host = "c0bbe3829ad14fe3b24e5c51247f57c1.s2.eu.hivemq.cloud";
+            _options.Port = 8883;
+            _options.UseTLS = true;
+
+            _options.UserName = "gantrymqtt"; //ik heb dit liever in een .env staan ;-;
+            _options.Password = "xC7gqKU6F!GZ#qM";
+
+            _client = new HiveMQClient(_options);
+
+            _activeGantry = activeGantry;
         }
 
         /* gantry pos calc */
@@ -97,11 +116,6 @@ namespace CraneSim.Core.Services
             return (double)(_gantryMoveStopwatch.ElapsedMilliseconds) / 1000;
         }
 
-        public Task SendMessage()
-        {
-            throw new NotImplementedException();
-        }
-
         public void StartStopwatch()
         {
             _gantryMoveStopwatch.Start();
@@ -113,15 +127,42 @@ namespace CraneSim.Core.Services
         }
 
         /* mqtt cnx */
+        
+        public async Task EstablishBrokerConnection()
+        {
+            var connectResult = await _client.ConnectAsync().ConfigureAwait(false);
 
-        public void Client_AfterConnect(object sender, AfterConnectEventArgs e)
+            await _client.SubscribeAsync("crane/components/gantry.command").ConfigureAwait(false);
+
+            _client.OnMessageReceived += Client_OnMessageReceived;
+        }
+
+        public async Task DisconnectBrokerConnection()
+        {
+            bool disconnectResult = await _client.DisconnectAsync().ConfigureAwait(false);
+        }
+
+        public Task SendMessage()
         {
             throw new NotImplementedException();
+        }
+        
+        public void Client_AfterConnect(object sender, AfterConnectEventArgs e)
+        {
+            _client.AfterConnect += Client_AfterConnect;
+            void Client_AfterConnect(object sender, AfterConnectEventArgs e)
+            {
+                //if you have recieved a message you can find it here, and do stuff when OnMessageRecieved.
+            }
         }
 
         public void Client_BeforeConnect(object sender, BeforeConnectEventArgs e)
         {
-            throw new NotImplementedException();
+            _client.BeforeConnect += Client_BeforeConnect;
+            void Client_BeforeConnect(object sender, BeforeConnectEventArgs e)
+            {
+                //do stuff when OnMessageRecieved.
+            }
         }
 
         public void Client_OnDisconnectRecieved(object sender, OnDisconnectReceivedEventArgs e)
@@ -131,17 +172,13 @@ namespace CraneSim.Core.Services
 
         public void Client_OnMessageReceived(object sender, OnMessageReceivedEventArgs e)
         {
-            throw new NotImplementedException();
+            _client.OnMessageReceived += Client_OnMessageReceived;
+            void Client_OnMessageReceived(object sender, OnMessageReceivedEventArgs e)
+            {
+                Console.WriteLine($"Recieved message: {e.PublishMessage.PayloadAsString}");
+                //if you have recieved a message you can find it here, and do stuff when OnMessageRecieved.
+            }
         }
 
-        public Task DisconnectBrokerConnection()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task EstablishBrokerConnection()
-        {
-            throw new NotImplementedException();
-        }
     }
 }
