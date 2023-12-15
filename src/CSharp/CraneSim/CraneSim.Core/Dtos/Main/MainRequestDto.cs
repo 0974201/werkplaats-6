ab016ab24015623e753f4712194ef13cabc88102
Andy Vandewalle <113889343+AndyHowest@users.noreply.github.com>
2023-12-15 10:34:15 +0100

    Add MainRequestDtos #42

---
+++ src/CSharp/CraneSim/CraneSim.Core/Dtos/Main/MainRequestDto.cs
@@ -0,0 +1,16 @@
+﻿using CraneSim.Core.Dtos.Meta;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace CraneSim.Core.Dtos.Main
+{
+    public class MainRequestDto
+    {
+        [JsonPropertyName("meta")]
+        public MetaRequestDto Meta { get; set; }
+    }
+}
