21b7c01268ecc41a800d49191f5bd1c041fc1731
Andy Vandewalle <113889343+AndyHowest@users.noreply.github.com>
2023-12-15 10:31:41 +0100

    Add HoistRequestDtos #42

---
+++ src/CSharp/CraneSim/CraneSim.Core/Dtos/Hoist/HoistRequestRelPosDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace CraneSim.Core.Dtos.Hoist
+{
+    public class HoistRequestRelPosDto
+    {
+        [JsonPropertyName("y")]
+        public float PositionY { get; set; }
+    }
+}
