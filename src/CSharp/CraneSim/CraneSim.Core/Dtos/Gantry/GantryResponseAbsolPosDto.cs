a98f2910a8a0d1fe3b6ee80cb9d24c63f27d022d
Andy Vandewalle <113889343+AndyHowest@users.noreply.github.com>
2023-12-15 10:53:08 +0100

    Add GantryResponseDtos #42

---
+++ src/CSharp/CraneSim/CraneSim.Core/Dtos/Gantry/GantryResponseAbsolPosDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace CraneSim.Core.Dtos.Crane
+{
+    public class GantryResponseAbsolPosDto
+    {
+        [JsonPropertyName("z")]
+        public float PositionZ { get; set; }
+    }
+}
