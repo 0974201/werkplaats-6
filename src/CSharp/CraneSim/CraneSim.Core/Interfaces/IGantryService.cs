653210d43c5c81b2435b20b39efb53ed7cef5190
Sushi <31491170+0974201@users.noreply.github.com>
2023-12-15 20:54:06 +0100

    Updated IGantryService.cs #38

--- src/CSharp/CraneSim/CraneSim.Core/Interfaces/IGantryService.cs
+++ src/CSharp/CraneSim/CraneSim.Core/Interfaces/IGantryService.cs
@@ -4,14 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CraneSim.Core.Entities;
+using HiveMQtt.Client.Events;
 
 namespace CraneSim.Core.Interfaces
 {
     internal interface IGantryService
     {
+        /* calculate pos */
+
         float CalculatePositiveMovement(Gantry entity);
         float CalculateNegativeMovement(Gantry entity);
         float CalculateCurrentSpeed(Gantry entity);
-        float CalculateAccelaration(Gantry entity);
+        float CalculateAcceleration(Gantry entity);
+        void ResetStopWatch();
+        void StartStopwatch();
+        void StopStopwatch();
+        double ReturnStopwatchvalue();
+        
+        /* mqtt cnx */
+        
+        Task EstablishBrokerConnection();
+        Task SendMessage();
+        Task DisconnectBrokerConnection();
+
+        void Client_BeforeConnect(object sender, BeforeConnectEventArgs e);
+        void Client_AfterConnect(object sender, AfterConnectEventArgs e);
+        void Client_OnMessageReceived(object sender, OnMessageReceivedEventArgs e);
+        void Client_OnDisconnectRecieved(object sender, OnDisconnectReceivedEventArgs e);
     }
 }
