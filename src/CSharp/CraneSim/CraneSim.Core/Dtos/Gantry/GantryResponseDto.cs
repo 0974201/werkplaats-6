4bb9ea2c9c081b063bff481db78f1f6f2dfe168e
Sushi <31491170+0974201@users.noreply.github.com>
2023-12-17 00:39:49 +0100

    updated GantryResponseDto.cs #38

--- src/CSharp/CraneSim/CraneSim.Core/Dtos/Gantry/GantryResponseDto.cs
+++ src/CSharp/CraneSim/CraneSim.Core/Dtos/Gantry/GantryResponseDto.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace CraneSim.Dtos.Gantry
@@ -9,12 +10,20 @@ namespace CraneSim.Dtos.Gantry
     public class GantryResponseDto
     {
         public int Id { get; set; }
+
+        [JsonPropertyName("component")]
         public string Name { get; set; }
-        
+
+        [JsonPropertyName("relativePosition")]
         public float PositionZ { get; set; } = 0.0F;
+
+        [JsonPropertyName("speed")]
         public float Speed { get; set; } = 0.0F;
+
+        [JsonPropertyName("acceleration")]
         public float Acceleration { get; set; } = 0.0F;
-        
+
+        [JsonPropertyName("active")]        
         public bool IsActive { get; set; } = false;
     }
 }
