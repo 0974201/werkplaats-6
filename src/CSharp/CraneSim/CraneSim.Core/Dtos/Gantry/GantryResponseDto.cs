eb6cb93fc492eeaca61bb57e74036037c944dec6
Sushi <31491170+0974201@users.noreply.github.com>
2023-12-16 23:55:42 +0100

    Adding GantryResponseDto.cs #38

---
+++ src/CSharp/CraneSim/CraneSim.Core/Dtos/Gantry/GantryResponseDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CraneSim.Dtos.Gantry
+{
+    public class GantryResponseDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+        public float PositionZ { get; set; } = 0.0F;
+        public float Speed { get; set; } = 0.0F;
+        public float Acceleration { get; set; } = 0.0F;
+        
+        public bool IsActive { get; set; } = false;
+    }
+}
