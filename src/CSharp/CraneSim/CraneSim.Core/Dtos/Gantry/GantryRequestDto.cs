714d68d4e16fd174fdf6eb09c213ac19b017b93c
Sushi <31491170+0974201@users.noreply.github.com>
2023-12-16 23:55:17 +0100

    Adding GantryRequestDto.cs #38

---
+++ src/CSharp/CraneSim/CraneSim.Core/Dtos/Gantry/GantryRequestDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CraneSim.Dtos.Gantry
+{
+    public class GantryRequestDto
+    { //https://learn.microsoft.com/en-us/aspnet/web-api/overview/data/using-web-api-with-entity-framework/part-5
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public float PositionZ { get; set; } = 0.0F;
+        public float Speed { get; set; } = 0.0F;
+        public float Acceleration { get; set; } = 0.0F;
+
+        public bool IsActive { get; set; } = false;
+    }
+}
