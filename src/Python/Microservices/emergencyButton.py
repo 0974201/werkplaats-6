bdbc33c1f458ac6e937c7bb164a5a04c8274b9b7
Yashin <1027048@hr.nl>
2023-12-16 04:56:37 +0100

    add comments

--- src/Python/Microservices/emergencyButton.py
+++ src/Python/Microservices/emergencyButton.py
@@ -1,15 +1,20 @@
+# class for the emergency button
 class EmergencyButton:
     def __init__(self, topic, sender):
-        self.topic = topic
-        self.sender = sender
+        self.topic = topic # The topic of the emergency message
+        self.sender = sender # The sender of the emergency message
 
+    # method to stop the system when the emergency button is pressed
     def stop_system(self):
         """
         Stops the entire system when the emergency button is pressed.
         """
+        # Print a message
         print(f"Emergency button pressed! Stopping the system for topic '{self.topic}' by sender '{self.sender}'.")
 
-# Test:
+# test the class
 if __name__ == "__main__":
+    # create an emergency button object with a topic and a sender
     emergency_button = EmergencyButton(topic="meta/emergency_button", sender="controller")
+    # call the stop_system method
     emergency_button.stop_system()
