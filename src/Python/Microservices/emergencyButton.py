610a9f8321763bc84011ce449429a0441ced99ee
Yashin <1027048@hr.nl>
2023-12-16 04:53:08 +0100

    add new method with given information

--- src/Python/Microservices/emergencyButton.py
+++ src/Python/Microservices/emergencyButton.py
@@ -2,3 +2,10 @@ class EmergencyButton:
     def __init__(self, topic, sender):
         self.topic = topic
         self.sender = sender
+
+    def stop_system(self):
+        """
+        Stops the entire system when the emergency button is pressed.
+        """
+        print(f"Emergency button pressed! Stopping the system for topic '{self.topic}' by sender '{self.sender}'.")
+
