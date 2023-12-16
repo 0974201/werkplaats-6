062d584050b1465ebf57ac7fb89bf65db0f5b566
Yashin <1027048@hr.nl>
2023-12-16 05:19:17 +0100

    add test to see if working correctly

--- src/Python/Microservices/boomMov.py
+++ src/Python/Microservices/boomMov.py
@@ -12,7 +12,7 @@ class Boom:
     self.positionY = positionY 
     self.rotationZ = rotationZ 
 
-      # method to move the boom up or down
+  # method to move the boom up or down
   def move(self, direction):
     # Check if the boom is active
     if self.isActive:
@@ -37,3 +37,23 @@ class Boom:
       self.rotationZ += angle
     else:
       print("The boom is not active. Please activate it first.")
+
+  # method to print the current state of the boom
+  def print_state(self):
+    # Print the attributes of the boom
+    print("isActive:", self.isActive)
+    print("Speed:", self.Speed)
+    print("positionX:", self.positionX)
+    print("positionY:", self.positionY)
+    print("rotationZ:", self.rotationZ)
+
+# instance of the boom class
+boom = Boom(True, 50, 100, 50, 0)
+# test the methods of the boom class
+boom.print_state() # Print the initial state
+boom.rotate(90) # Rotate the boom 
+boom.print_state() # Print the updated state
+boom.move("up") # Move the boom up
+boom.print_state() 
+boom.move("down") 
+boom.print_state() 
