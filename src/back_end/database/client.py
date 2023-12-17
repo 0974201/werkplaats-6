ab21dfe66baf201acb201119380339fab0c389d8
miroaccgh <mironoordzij@outlook.comD>
2023-12-17 02:01:36 +0100

    add functionality database checks for topic of message and creates a collactions for it or catogorises it to that collection

--- src/back_end/database/client.py
+++ src/back_end/database/client.py
@@ -13,23 +13,25 @@ class Client:
         self.certificate = os.path.join(os.path.dirname(__file__), "certificate/admin.pem")
         self.client = MongoClient(self.uri, tls=True, tlsCertificateKeyFile=self.certificate)
         self.database = self.client["st-2324-1-d-wx1-t2-2324-wx1-bear"]
-        self.collection = self.database.crane_state
 
     def insert_document(self, msg: str) -> dict:
         timezone = pytz.timezone("Europe/Amsterdam")
         datetime_insertion = datetime.now(timezone)
 
+        msg_object = ast.literal_eval(msg)
+        topic = msg_object["meta"]["topic"]
+
         formatted_datetime_insertion = datetime_insertion.strftime("%Y-%m-%d/%H:%M:%S")
         document = {
             "datetime": formatted_datetime_insertion,
-            "msg": ast.literal_eval(msg)
+            "msg": msg_object
         }
         insertion = {
             "inserted": bool,
             "document": object
         }
         try:
-            insert_one_result = self.collection.insert_one(document)
+            insert_one_result = self.database[topic].insert_one(document)
             insertion["document_id"] = insert_one_result.inserted_id
             insertion["inserted"] = True
         except Exception as error:
