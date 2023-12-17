ff82e0f62259a490d94d9a4adcac545dd5483510
Yashin <1027048@hr.nl>
2023-12-17 04:12:02 +0100

    implement mqtt to code #46

--- src/Python/Microservices/emergencyButton.py
+++ src/Python/Microservices/emergencyButton.py
@@ -1,20 +1,28 @@
+import paho.mqtt.client as mqtt
+from paho import mqtt
+import json
+
 # class for the emergency button
 class EmergencyButton:
-    def __init__(self, topic, sender):
-        self.topic = topic # The topic of the emergency message
-        self.sender = sender # The sender of the emergency message
+    def __init__(self, mqtt_broker, topic, sender):
+        self.topic = topic # topic of emergency message
+        self.sender = sender # sender of emergency message
+
+        # mqtt connection
+        self.client = mqtt.Client()
+        self.client.connect(mqtt_broker)
+        self.client.loop_start()
 
     # method to stop the system when the emergency button is pressed
     def stop_system(self):
-        """
-        Stops the entire system when the emergency button is pressed.
-        """
-        # Print a message
+        # print message
         print(f"Emergency button pressed! Stopping the system for topic '{self.topic}' by sender '{self.sender}'.")
+        # create message
+        message = json.dumps({"command": "stop_system"})
+        # publish it to the topic
+        self.client.publish(self.topic, message)
 
-# test the class
-if __name__ == "__main__":
-    # create an emergency button object with a topic and a sender
-    emergency_button = EmergencyButton(topic="meta/emergency_button", sender="controller")
-    # call the stop_system method
-    emergency_button.stop_system()
+    # stop the mqtt client
+    def stop_mqtt(self):
+        self.client.loop_stop()
+        self.client.disconnect()
