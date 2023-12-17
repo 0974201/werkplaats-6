150a8272f9240914193079b91163d6ebc47d4835
Yashin <1027048@hr.nl>
2023-12-17 04:13:36 +0100

    add errorhandling #46

--- src/Python/Microservices/emergencyButton.py
+++ src/Python/Microservices/emergencyButton.py
@@ -10,8 +10,11 @@ class EmergencyButton:
 
         # mqtt connection
         self.client = mqtt.Client()
-        self.client.connect(mqtt_broker)
-        self.client.loop_start()
+        try:
+            self.client.connect(mqtt_broker)
+            self.client.loop_start()
+        except Exception as e:
+            print(f"Failed to connect to MQTT broker: {e}")
 
     # method to stop the system when the emergency button is pressed
     def stop_system(self):
@@ -19,10 +22,13 @@ class EmergencyButton:
         print(f"Emergency button pressed! Stopping the system for topic '{self.topic}' by sender '{self.sender}'.")
         # create message
         message = json.dumps({"command": "stop_system"})
-        # publish it to the topic
+        # publish it to the topic 
         self.client.publish(self.topic, message)
 
     # stop the mqtt client
     def stop_mqtt(self):
-        self.client.loop_stop()
-        self.client.disconnect()
+        try:
+            self.client.loop_stop()
+            self.client.disconnect()
+        except Exception as e:
+            print(f"Failed to stop MQTT: {e}")
