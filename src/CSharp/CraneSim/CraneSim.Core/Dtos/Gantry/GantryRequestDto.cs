7908fda2b2d5039e796cc6fae6c096c69b54a75c
Sushi <31491170+0974201@users.noreply.github.com>
2023-12-17 22:24:52 +0100

    Updated GantryRequestDto.cs #38

--- src/CSharp/CraneSim/CraneSim.Core/Dtos/Gantry/GantryRequestDto.cs
+++ src/CSharp/CraneSim/CraneSim.Core/Dtos/Gantry/GantryRequestDto.cs
@@ -4,13 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using CraneSim.Core.Dtos.Gantry;
 
 namespace CraneSim.Dtos.Gantry
 {
     public class GantryRequestDto
     { //https://learn.microsoft.com/en-us/aspnet/web-api/overview/data/using-web-api-with-entity-framework/part-5
 
-        [JsonPropertyName("topic")]
-        public string Topic { get; set; }
+        [JsonPropertyName("meta")]
+        public GantryRequestMetaDto Meta { get; set; }
+        [JsonPropertyName("msg")]
+        public GantryRequestMsgDto Msg { get; set; }
     }
 }
