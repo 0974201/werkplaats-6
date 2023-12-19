7ba75f3bb1a7a94c7d031ca8a085fd2089d09e24
Yashin <1027048@hr.nl>
2023-12-19 21:45:49 +0100

    add different keyfunctions, also updated data

--- src/back_end/emergencyButton.py
+++ src/back_end/emergencyButton.py
@@ -5,35 +5,52 @@ import json
 
 # class for the emergency button
 class EmergencyButton:
-    def __init__(self, isActive):
-        self.client = broker.client.Client("emergencyButton", [("crane/components/emergencyButton/state")], 0)
+    def __init__(self, isPressed):
+        self.client = broker.client.Client("emergencyButton", [("meta/emergency_button")], 0)
         self.client.serve()
-        self.isActive = isActive
+        self.isPressed = isPressed
         self.topic = "meta/emergency_button" # topic of emergency message
         self.sender = "controller" # sender of emergency message
+        self.loop = True
+
+    def on_message(self, client, userdata, msg):
+        print("Message received: ", msg.payload)
+        data_dict = json.loads(msg.payload.decode('utf-8'))
+        keys = data_dict.get('msg', {}).get('key') 
+        self.stop_system(keys)
+
 
     # method to stop the system when the emergency button is pressed
-    def stop_system(self):
-        # print message
-        print(f"Emergency button pressed! Stopping the system for topic '{self.topic}' by sender '{self.sender}'.")
-        if self.isActive:
-            self.emergencyData()
+    def stop_system(self, keys = None):
+        while self.loop:
+            if keys == 2:
+                if self.isPressed:
+                    print("Emergency button already pressed!")
+                else:
+                    # print message
+                    print(f"Emergency button pressed! Stopping the system for topic '{self.topic}' by sender '{self.sender}'.")
+                    self.emergencyData()
+            elif keys == 1:
+                if self.isPressed:
+                    self.Pressed = False
+                    print("Emergency button resetted.")
+                else:
+                    print("Emergency button is not active.")
+            else:
+                print("No key pressed!")
+                
 
     def emergencyData(self):
         data = {
             "meta":
                 {
-                    "topic": "crane/components/emergencyButton/state",
-                    "isActive": self.isActive,
-                    "component": "emergencyButton"
+                    "topic": self.topic
                 },
             "msg": {
-                "command": "stop_system",
-                "topic": self.topic,
-                "sender": self.sender
+                "isPressed": self.isPressed,
                 }
             }
-        self.client.publish("crane/components/emergencyButton/state", data)
+        self.client.publish("meta/emergency_button", data)
         self.client.disconnect()
 
 emergency = EmergencyButton(True)
