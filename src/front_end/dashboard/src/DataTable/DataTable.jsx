debeba0fab7da6744fabb28c0e2ff1749340f3d5
Sushi <31491170+0974201@users.noreply.github.com>
2023-12-19 21:17:57 +0100

    "Updated" DataTable.jsx #23
    idk either mane
    
    Signed-off-by: Sushi <31491170+0974201@users.noreply.github.com>

--- src/front_end/dashboard/src/DataTable/DataTable.jsx
+++ src/front_end/dashboard/src/DataTable/DataTable.jsx
@@ -3,19 +3,38 @@ import './DataTable.css'
 
 export default function DataTable(props) {
 
+    let craneArray = [];
+    Object.values(props.craneInfo).flat().forEach(item => {
+        Object.entries(item).forEach(([k, v]) => {
+            craneArray.push(k + "" + v)
+        })
+    })
+
+    console.log(craneArray)
+
     return (
         <>
             {console.log(props.craneInfo)}
+            {console.log(Object.keys(props.craneInfo))}
+            {console.log(Object.values(props.craneInfo)) /* hij is nested lol*/ } 
             <div className='datatable_container'>
                 <table className='datatable_table'>
                     <tbody>
                         <tr>
                             <th>table</th>
                         </tr>
-                        {Object.keys(props.craneInfo).map(key => {
+                        {/*Object.keys(props.craneInfo).map(key => {
                             return (
                                 <tr key={key}>
                                     <td>{key}</td>
+                                    <td>{key}</td>
+                                    <td>{key}</td>
+                                </tr>
+                            )
+                        })*/}
+                        {craneArray.map(key => {
+                            return (
+                                <tr key={key}>
                                     <td>{key}</td>
                                 </tr>
                             )
