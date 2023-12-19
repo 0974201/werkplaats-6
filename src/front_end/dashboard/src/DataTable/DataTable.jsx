8ab831834840ee7bc6207d5cda69aa07a90050f7
Sushi <31491170+0974201@users.noreply.github.com>
2023-12-19 21:34:11 +0100

    i give up #23
    Signed-off-by: Sushi <31491170+0974201@users.noreply.github.com>

--- src/front_end/dashboard/src/DataTable/DataTable.jsx
+++ src/front_end/dashboard/src/DataTable/DataTable.jsx
@@ -6,42 +6,33 @@ export default function DataTable(props) {
     let craneArray = [];
     Object.values(props.craneInfo).flat().forEach(item => {
         Object.entries(item).forEach(([k, v]) => {
-            craneArray.push(k + "" + v)
+            craneArray.push(k + v)
         })
-    })
+    });
 
-    console.log(craneArray)
+    console.log(craneArray);
+    console.log(props.craneInfo);
+    console.log(Object.keys(props.craneInfo));
+    console.log(Object.values(props.craneInfo)); // hij is nested lol
 
     return (
-        <>
-            {console.log(props.craneInfo)}
-            {console.log(Object.keys(props.craneInfo))}
-            {console.log(Object.values(props.craneInfo)) /* hij is nested lol*/ } 
-            <div className='datatable_container'>
-                <table className='datatable_table'>
-                    <tbody>
-                        <tr>
-                            <th>table</th>
+    <>
+    <div className='datatable_container'>
+        <table className='datatable_table'>
+            <tbody>
+                <tr>
+                    <th>table</th>
+                </tr>
+                {craneArray.map(key => {
+                    return (
+                        <tr key={key}>
+                            <td>{key}</td>
                         </tr>
-                        {/*Object.keys(props.craneInfo).map(key => {
-                            return (
-                                <tr key={key}>
-                                    <td>{key}</td>
-                                    <td>{key}</td>
-                                    <td>{key}</td>
-                                </tr>
-                            )
-                        })*/}
-                        {craneArray.map(key => {
-                            return (
-                                <tr key={key}>
-                                    <td>{key}</td>
-                                </tr>
-                            )
-                        })}
-                    </tbody>
-                </table>
-            </div>
-        </>
+                    )
+                })}
+            </tbody>
+        </table>
+        </div>
+    </>
     )
 }
