959b3c4e7e95d304ba1cc7e6c7e6542424f779b8
zashastigt <0925315@hr.nl>
2023-12-20 10:31:24 +0100

    Add bach file #119

--- run/windows.bat
+++ run/windows.bat
@@ -5,10 +5,4 @@ CD ../src/front_end/dashboard
 START npm install
 START npm run dev
 
-"C:\Users\zstig\AppData\Local\Programs\Python\Python310\python.exe"
-"C:\Users\zstig\WebstormProjects\st-2324-1-d-wx1-t2-2324-wx1-bear\src\back_end\main.py"
-:: Start Python
-:: CD ../../back_end
-:: DIR
-:: START main.py
-echo Crane Started
+echo Dashboard Started
