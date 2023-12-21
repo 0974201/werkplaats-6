382c5920e974a192808c8218844e4878d9204b95
quylian <90244189+TOA4ASA@users.noreply.github.com>
2023-12-21 16:44:43 +0100

    hoist iddue with on_message

--- src/back_end/hoist.py
+++ src/back_end/hoist.py
@@ -3,42 +3,50 @@ import random
 import time
 import json
 import math
+import keyboard
 
 class Hoist:
     def __init__(self):
         self.loop = True
-        self.client = broker.client.Client("hoist", [("crane/components/hoist/command", 0)])
+        self.client = broker.client.Client("hoist", [("crane/components/hoist/command", 0),("trolly", 0),("gantry", 0),("container", 0),("dashboard", 0)])
         self.client.serve()
-        self.is_active = []
+        self.is_active = True
         self.acceleration = 1
         self.frequency = 0.1
         self.hoist_y = 0
-        self.movement()
+        self.looper()
 
     def on_message(self, client, userdata, msg):
         print("Message received: ", msg.payload)
-        data_dict = json.loads(msg.payload.decode('utf-8'))
-
+        data_dict = json.loads(msg.payload.decode('utf-8'))  
+        self.proccesData(data_dict)
+    
+    broker.client.Client.on_message = on_message
+    
+    def proccesData(self, data_dict):
         isConnected = data_dict.get('msg', {}).get('isConnected')
-        self.is_active = data_dict.get('meta', {}).get('isActive')
-        keys = data_dict.get('msg', {}).get('key') 
+        is_active = data_dict.get('meta', {}).get('isActive')
+        command = data_dict.get('msg', {}).get('command', {}).get('hoist')
         containerX = data_dict.get('msg', {}).get('containerX') 
         containerY = data_dict.get('msg', {}).get('containerY') 
         containerZ = data_dict.get('msg', {}).get('containerZ')
         hoistX = data_dict.get('msg', {}).get('TrollyX')
         hoistZ = data_dict.get('msg', {}).get('Gantryz')
         
-        self.movement(keys)
+        print(command)
+        self.movement(command)
         self.container_connect(isConnected, containerX, containerY, containerZ, hoistX, hoistZ)
 
-    broker.client.Client.on_message = on_message
+    
 
-    def movement(self, keys= None):
+    def looper(self):
+        while self.loop:
+            pass
+
+    def movement(self, command):
         while self.loop == True:
-            keys = 0
             time.sleep(self.frequency)
-            if keys == 2:
-                print(f"keys: {keys}")
+            if command == 1:
                 
                 self.acceleration = self.acceleration * 1.1
                 speed = 0.83 * self.acceleration
@@ -51,8 +59,8 @@ class Hoist:
                 
                 self.position(move_hoist_y)
 
-            elif keys == 1:
-                print(f"keys: {keys}")
+            elif command == -1:
+                print(f"command: {command}")
                 self.acceleration = self.acceleration * 1.1
                 speed = -0.83 * self.acceleration
                 print(f"speed: {speed}")
@@ -65,7 +73,7 @@ class Hoist:
                 
                 self.position(move_hoist_y)
             else:
-                print("no key press")
+                print("no_keypress")
 
     def position(self, speed):
         move_hoist_y = speed
@@ -95,11 +103,12 @@ class Hoist:
                 isConnected = True
             else:
                 print("to far away")
+                isConnected = False
             print(connected)
             self.Hoist_data(isConnected)
 
 
-    def Hoist_data(self, speed, pos, isConnected):  
+    def Hoist_data(self, speed, pos):  
         data = {
             "meta":
                 {
@@ -108,7 +117,7 @@ class Hoist:
                     "component": "hoist"
                 },
             "msg": {
-                "isConnected": isConnected,
+                "isConnected": "isConnected",
                 "relativePosition": {
                     "HoistY": pos
                 },
@@ -125,6 +134,7 @@ class Hoist:
                 }
             }
         }
+        print(data)
         self.client.publish("crane/components/hoist/state", data)
-        # self.client.disconnect()
+        self.client.disconnect()
 Hoist()
