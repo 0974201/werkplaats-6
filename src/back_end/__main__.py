d66493d3da933d6104d2a46415997b8a92c02c59
miroaccgh <mironoordzij@outlook.comD>
2023-12-31 15:23:15 +0100

    add error assertion for importing package

--- src/back_end/__main__.py
+++ src/back_end/__main__.py
@@ -4,3 +4,5 @@ from . import db_mqtt_client
 if __name__ == "__main__":
     frequency = 0.1
     database_api = db_mqtt_client.DbMqttClient(frequency)
+else:
+    raise "running package as import is not supported"
